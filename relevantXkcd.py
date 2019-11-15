29d27759fddd4a15cf5cb6e6ba3161c2d84568ba
Ludi Wang <neopteryx@ludis-mbp.lan>
2019-11-14 21:44:56 -0800

    Add search by phrase feature

--- relevantXkcd.py
+++ relevantXkcd.py
@@ -1,4 +1,5 @@
 import webbrowser
+from googlesearch import search
 
 url = "https://xkcd.com/"
 searchType = input("Do you want to search by NUMBER or by PHRASE?").lower()
@@ -12,3 +13,17 @@ if searchType == "number":
     except:
         print("An error occured. Perhaps you didn't enter an integer, or you don't have a recognized web browser, or dark magic interfered. The world may never know.")
         webbrowser.open_new("https://xkcd.com/2200/")
+elif searchType == "phrase":
+    referencePhrase = input("What is the relevant phrase?")
+    results = input("How many xkcds would you like to search for?")
+    query = ("xkcd " + str(referencePhrase))
+    try:
+        num = int(results)
+        for i in search(query, tld='co.in', num=num, stop=num, pause=5):
+            str(i)
+            webbrowser.open_new(i)
+    except:
+        print("An error occured. Perhaps you didn't enter an integer for the number of searches, or you don't have google, or maybe its all my fault and I really am a disappointment like my parents said.")
+        webbrowser.open_new("https://xkcd.com/2200/")
+
+
