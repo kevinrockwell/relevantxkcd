29230a8ee2ba93e6b846c03cd41a478d2915c22a
Ludi Wang <neopteryx@Ludis-MacBook-Pro.local>
2019-11-14 20:03:43 -0800

    Improve error message
    Also links to xkcd 2200

--- relevantXkcd.py
+++ relevantXkcd.py
@@ -10,8 +10,5 @@ if searchType == "number":
         url = (f'{url}{comicNumber}')
         webbrowser.open_new(url)
     except:
-        print("Must be an integer.")
-    
-    
-
-
+        print("An error occured. Perhaps you didn't enter an integer, or you don't have a recognized web browser, or dark magic interfered. The world may never know.")
+        webbrowser.open_new("https://xkcd.com/2200/")
