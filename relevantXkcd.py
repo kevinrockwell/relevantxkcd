d72567cdf03915f69b90ff7fdd4841731db21f75
kevinr03 <kevin.n.rockwell@gmail.com>
2019-11-17 21:55:06 -0800

    Add regex matching to search
    -filters domain filtered results for only the comics
    -Tweaked search args a bit

--- relevantXkcd.py
+++ relevantXkcd.py
@@ -1,4 +1,5 @@
 import webbrowser
+import re
 from googlesearch import search
 
 url = "https://xkcd.com/"
@@ -14,14 +15,18 @@ if searchType == "number":
         print("An error occured. Perhaps you didn't enter an integer, or you don't have a recognized web browser, or dark magic interfered. The world may never know.")
         webbrowser.open_new("https://xkcd.com/2200/")
 elif searchType == "phrase":
+    pattern = r"https?://xkcd.com/\d+/" # Matches url for an xkcd, with or without https
     referencePhrase = input("What is the relevant phrase?")
-    results = input("How many xkcds would you like to search for?")
+    num_results = input("How many xkcds would you like to search for?")
     query = ("site:xkcd.com " + str(referencePhrase))
     try:
-        num = int(results)
-        for i in search(query, tld="co.in", num=num, stop=num, pause=5):
-            str(i)
-            webbrowser.open_new(i)
+        i = int(num_results)
+        for result in search(query, num=20):
+            if i <= 0:
+                break
+            if re.match(pattern, result) is not None: # Result matches pattern
+                i -= 1
+                webbrowser.open_new(result)
     except:
         print("An error occured. Perhaps you didn't enter an integer for the number of searches, or you don't have google, or maybe its all my fault and I really am a disappointment like my parents said.")
         webbrowser.open_new("https://xkcd.com/2200/")
